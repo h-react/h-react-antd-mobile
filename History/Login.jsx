b1b4f5e0fb76646617d0fd34277f53d9c80afa00
hunzsig <mzyhaohaoren@qq.com>
2020-09-23 00:50:09 +0800

    chore(*): new api

--- History/Login.jsx
+++ History/Login.jsx
@@ -18,7 +18,7 @@ class Login extends Component {
   componentDidMount = () => {
     if (this.isWechat) {
       Api.query().post({SDK_WXMP_OAUTH: this.search.code ? this.search : {}}, (res) => {
-        if (res.code === 200) {
+        if (res.error === 0) {
           if (res.data.string) {
             window.location = res.data.string; //微信跳转
           } else {
